445504cb4721312f52aafc0026bb7c4d156da0c2
Ilia <istudyatuni@gmail.com>
2022-01-29 17:30:32 +0300

    Add navbar

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,9 @@
 <script context="module">
 	import 'src/tailwind.svelte'
 
+	import { Navbar } from '@minetower/ui-components'
 	import ResourcePacks from 'src/components/pages/ResourcePacks.svelte'
 </script>
 
+<Navbar title="Minecraft Tweaks" repo_url="/minecraft-tweaks" center_title />
 <ResourcePacks />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
+// yes, hardcode
+const ui_components = ['Navbar']
+
 module.exports = {
-	content: ['./src/**/*.svelte'],
-	theme: {},
+	content: [
+		'./src/**/*.svelte',
+		...ui_components.map((c) =>
+			require.resolve(`@minetower/ui-components/src/${c}.svelte`)
+		),
+	],
+	theme: {
+		extend: {},
+	},
 	variants: {},
 	plugins: [],
 }
