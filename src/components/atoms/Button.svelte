69322a6e89c8fa9eab06df92656836acef43a4f5
Ilia <istudyatuni@gmail.com>
2022-02-02 18:29:17 +0300

    Use class instead of className in button

--- src/components/atoms/Button.svelte
+++ src/components/atoms/Button.svelte
@@ -1,12 +1,11 @@
 <script>
-	export let className = '',
-		disabled = false
+	export let disabled = false
 </script>
 
 <button
 	type="button"
 	on:click
-	class="py-2 px-3 bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-300 text-white font-semibold rounded-md focus:outline-none {className}"
+	class="py-2 px-3 bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-300 text-white font-semibold rounded-md focus:outline-none {$$props.class}"
 	class:cursor-pointer={!disabled}
 	{disabled}>
 	<slot />
