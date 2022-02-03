0088e0e04820ac1a7fea0bd5ac6fe6769887dc86
Ilia <istudyatuni@gmail.com>
2022-02-03 10:06:14 +0300

    fix: cursor pointer on whole file input block (not works fully in FF)

--- src/components/atoms/FileInput.svelte
+++ src/components/atoms/FileInput.svelte
@@ -2,28 +2,31 @@
 	export let text = 'Select a file',
 		files = null
 
-	let input = null
-
 	$: displayed = files ? files[0].name : text
 </script>
 
 <div class="mx-auto mt-3">
 	<div class="relative rounded">
 		<div
-			class="relative border border-gray-200 border-dashed rounded"
-			on:click={() => input.click()}>
+			class="relative border border-gray-200 border-dashed rounded cursor-pointer">
 			<input
 				accept=".zip"
 				type="file"
 				bind:files
 				on:change
-				bind:this={input}
-				class="hidden"
+				class="absolute w-full h-full opacity-0 z-50"
 				title="" />
 
-			<div class="py-1 px-2 text-center cursor-pointer">
-				<p>{displayed}</p>
+			<div class="py-1 px-2 text-center">
+				<p class="m-0">{displayed}</p>
 			</div>
 		</div>
 	</div>
 </div>
+
+<style>
+	input[type='file'],
+	input[type='file']::-webkit-file-upload-button {
+		@apply cursor-pointer;
+	}
+</style>
