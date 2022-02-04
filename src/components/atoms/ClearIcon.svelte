4b9323d1faf2903e6963851b008ea1fd8f0b9ef0
Ilia <istudyatuni@gmail.com>
2022-02-04 13:51:34 +0300

    feat: add tooltip on clear button

--- src/components/atoms/ClearIcon.svelte
+++ src/components/atoms/ClearIcon.svelte
@@ -1,10 +1,24 @@
-<img
-	on:click
-	src="/assets/images/icons/clear.svg"
-	class="m-0 p-0 cursor-pointer"
-	title="Clear"
-	tabindex="0"
-	alt="clear" />
+<script context="module">
+	import Tooltip from 'src/components/atoms/Tooltip.svelte'
+</script>
+
+<script>
+	let show = false
+</script>
+
+<Tooltip center {show}>
+	<svelte:fragment slot="tooltip">Clear</svelte:fragment>
+
+	<img
+		on:click
+		slot="content"
+		on:focus={() => (show = true)}
+		on:blur={() => (show = false)}
+		src="/assets/images/icons/clear.svg"
+		class="cursor-pointer"
+		tabindex="0"
+		alt="clear" />
+</Tooltip>
 
 <style>
 	img {
