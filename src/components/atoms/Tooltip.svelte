a082366f3d40eb33ef50a0cd06c011f93f569e31
Ilia <istudyatuni@gmail.com>
2022-02-04 15:25:05 +0300

    feat: add ability to center tooltip vertically
    now without freezes

--- src/components/atoms/Tooltip.svelte
+++ src/components/atoms/Tooltip.svelte
@@ -1,18 +1,48 @@
 <!-- https://github.com/Cosbgn/tailwindcss-tooltips -->
+<script context="module">
+	const calcCenterMargin = (left, right) =>
+		`calc(-${left}px / 2 + ${right}px / 2)`
+</script>
+
 <script>
 	/** @type {Boolean} Show tooltip at all or not */
 	export let has_tooltip = true,
 		/** @type {Boolean} Show tooltip programmatically */
-		show = false
+		show = false,
+		/** @type {Boolean} Center tooltip vertically */
+		center = false
+
+	let tooltipEl,
+		contentEl,
+		margin_left,
+		centered = false
+
+	// calc margin on first 'show' change
+	$: if (show) setMargin()
+
+	function setMargin() {
+		if (!centered && center) {
+			margin_left = calcCenterMargin(
+				tooltipEl.offsetWidth,
+				contentEl.clientWidth
+			)
+		}
+		centered = true
+	}
 </script>
 
 <div class:has-tooltip={has_tooltip}>
 	<span
 		class:show
+		bind:this={tooltipEl}
+		style:margin-left={margin_left}
 		class="tooltip rounded shadow-lg py-1 px-2 bg-gray-100 text-sm text-gray-900 -mt-8">
 		<slot name="tooltip" />
 	</span>
-	<slot name="content" />
+
+	<div bind:this={contentEl} on:mouseenter|once={setMargin}>
+		<slot name="content" />
+	</div>
 </div>
 
 <style>
