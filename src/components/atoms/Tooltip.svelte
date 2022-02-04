b9804cf1a9db9331cdc62b00b3d74c7bc85e0a14
Ilia <istudyatuni@gmail.com>
2022-02-04 13:48:10 +0300

    feat: add ability to center tooltip vertically

--- src/components/atoms/Tooltip.svelte
+++ src/components/atoms/Tooltip.svelte
@@ -3,12 +3,19 @@
 	/** @type {Boolean} Show tooltip at all or not */
 	export let has_tooltip = true,
 		/** @type {Boolean} Show tooltip programmatically */
-		show = false
+		show = false,
+		/** @type {Boolean} Center tooltip vertically */
+		center = false
+
+	let width
+	$: margin_left = center ? `calc(-${width}px / 4)` : ''
 </script>
 
 <div class:has-tooltip={has_tooltip}>
 	<span
 		class:show
+		bind:clientWidth={width}
+		style:margin-left={margin_left}
 		class="tooltip rounded shadow-lg py-1 px-2 bg-gray-100 text-sm text-gray-900 -mt-8">
 		<slot name="tooltip" />
 	</span>
