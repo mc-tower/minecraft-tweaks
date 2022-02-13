4f8262b745526b48aa0329566a549374e4e0d7c8
Ilia <istudyatuni@gmail.com>
2022-02-13 11:39:41 +0300

    feat: add basic modal

---
+++ src/components/atoms/Modal.svelte
@@ -0,0 +1,48 @@
+<script context="module">
+	import { fly, slide } from 'svelte/transition'
+
+	import ClearIcon from 'src/components/atoms/ClearIcon.svelte'
+
+	function noop() {}
+</script>
+
+<script>
+	export let show = false
+
+	function open() {
+		show = true
+	}
+	function close() {
+		show = false
+	}
+</script>
+
+<span on:click={open}>
+	<slot name="trigger" />
+</span>
+
+{#if show}
+	<div
+		class="fixed inset-0 w-full h-full z-20 bg-black bg-opacity-50 overflow-y-auto"
+		on:click={close}
+		transition:fly>
+		<div
+			class="relative sm:w-3/4 md:w-1/2 lg:w-1/3 mx-2 sm:mx-auto my-10 opacity-100">
+			<div
+				class="relative bg-slate-800 shadow-lg rounded-md text-neutral-300 z-20"
+				on:click|stopPropagation={noop}
+				transition:slide>
+				<header class="flex items-center justify-between p-2 pt-4">
+					<h2 class="font-semibold px-2">
+						<slot name="header">Header</slot>
+					</h2>
+					<ClearIcon on:click={close} />
+				</header>
+				<main class="px-2 pb-6 text-center">
+					<slot name="body">Body</slot>
+				</main>
+				<!-- <footer class="flex justify-center p-2"></footer> -->
+			</div>
+		</div>
+	</div>
+{/if}
