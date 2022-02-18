b5abcd15ff9c042f0ffa4e20002ac7ed16b80d12
Ilia <istudyatuni@gmail.com>
2022-02-18 22:28:40 +0300

    refactor: move global style from component

--- src/components/molecules/DescriptionModal.svelte
+++ src/components/molecules/DescriptionModal.svelte
@@ -14,12 +14,3 @@
 	<svelte:fragment slot="header">{$description.name}</svelte:fragment>
 	<svelte:fragment slot="body">{@html $description.content}</svelte:fragment>
 </Modal>
-
-<style>
-	/* for {@html} */
-	:global(code, pre) {
-		padding: 3px 5px;
-		@apply rounded;
-		@apply bg-slate-700;
-	}
-</style>
